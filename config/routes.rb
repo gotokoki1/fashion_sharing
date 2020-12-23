0d879c1cdcbc6f8ed146a2565f2374272284f526
gotokoki1 <72792963+gotokoki1@users.noreply.github.com>
2020-12-24 00:14:10 +0900

    コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   get 'posts/index'
   root to: "posts#index"
   resources :posts do
     resources :comments, only: [:create]
   end
+  resources :users, only: [:show]
 end
