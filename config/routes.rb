ef2d5a979fc9ed54a29051892af455f5578ff8bd
gotokoki1 <72792963+gotokoki1@users.noreply.github.com>
2021-01-08 19:43:48 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
   root to: "posts#index"
+
   resources :posts do
     resources :comments, only: [:create]
   end
+
+  resources :posts do
+    resource :post_likes, only: [:create, :destroy]
+  end
+
   resources :users, only: [:show, :edit, :update, :destroy]
 end
